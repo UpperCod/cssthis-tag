67dfe0c7c2d20253a947f8b90aef92336c484352
Matias Trujillo <mtrujillo@outlook.cl>
2018-09-11 22:41:24 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-export default function(id, props) {
-    let style =
-        document.querySelector(`style#${id}`) ||
-        document.createElement("style");
+export default function(id, props = {}) {
+    let $ = document,
+        style = $.querySelector(`style#${id}`) || $.createElement("style");
     style.id = id;
     props = { id, cn: id, ...props };
+    $.head.appendChild(style);
     return css =>
         (style.innerHTML = []
             .concat(css)
